7265dd4546373dfcc44052474097b299ec3ecf68
Sadiq Ahmed <154236087+sadiqahmed2001@users.noreply.github.com>
2024-05-18 19:30:48 +0530

    Add files via upload

---
+++ password_generator/random_password.py
@@ -0,0 +1,14 @@
+import random
+
+import string
+
+pass_len=12
+charvalue=string.ascii_letters+string.digits+string.punctuation
+
+password=""
+for i in range(pass_len):
+    password += random.choice(charvalue)
+
+#list comprehension password="".join([random.choice(charvalue) for i in range(pass_len)])
+#print(res)
+print("your random password is: ", password)
