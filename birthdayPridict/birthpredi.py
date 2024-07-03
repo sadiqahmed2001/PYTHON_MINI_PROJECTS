fae897a3642a8f15f413385bd0edb17cf76c9cac
Sadiq Ahmed <154236087+sadiqahmed2001@users.noreply.github.com>
2024-07-03 16:12:05 +0530

    Update birthpredi.py

--- birthdayPridict/birthpredi.py
+++ birthdayPridict/birthpredi.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd   
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
